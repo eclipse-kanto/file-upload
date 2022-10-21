26888e6b72a9617574f8d6db9b2376c162163bd8
Georgi Boyvalenkov <Georgi.Boyvalenkov@bosch.io>
2022-10-21 17:26:17 +0300

    [#58] Provide integration tests for the key functionalities
    - some basic testing
    
    Signed-off-by: Georgi Boyvalenkov <Georgi.Boyvalenkov@bosch.io>

---
+++ integration/request_util.go
@@ -0,0 +1,117 @@
+// Copyright (c) 2022 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+
+package integration
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func (suite *uploadSuite) doRequest(method string, url string, params map[string]interface{}) ([]byte, error) {
+	var body io.Reader
+	var err error
+	if params != nil {
+		jsonValue, err := json.Marshal(params)
+		if err != nil {
+			return nil, err
+		}
+		body = bytes.NewBuffer(jsonValue)
+	}
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	if params != nil {
+		req.Header.Add("Content-Type", "application/json")
+	}
+
+	req.SetBasicAuth(suite.cfg.DittoUser, suite.cfg.DittoPassword)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return nil, fmt.Errorf("%s %s request failed: %s", method, url, resp.Status)
+	}
+
+	return io.ReadAll(resp.Body)
+}
+
+func getThingConfig(mqttClient mqtt.Client) (*thingConfig, error) {
+	type result struct {
+		cfg *thingConfig
+		err error
+	}
+
+	ch := make(chan result)
+
+	if token := mqttClient.Subscribe("edge/thing/response", 1, func(client mqtt.Client, message mqtt.Message) {
+		var cfg thingConfig
+		if err := json.Unmarshal(message.Payload(), &cfg); err != nil {
+			ch <- result{nil, err}
+		}
+		ch <- result{&cfg, nil}
+	}); token.Wait() && token.Error() != nil {
+		return nil, token.Error()
+	}
+
+	if token := mqttClient.Publish("edge/thing/request", 1, false, ""); token.Wait() && token.Error() != nil {
+		return nil, token.Error()
+	}
+
+	timeout := 5 * time.Second
+	select {
+	case result := <-ch:
+		return result.cfg, result.err
+	case <-time.After(timeout):
+		return nil, fmt.Errorf("thing config not received in %v", timeout)
+	}
+}
+
+func (suite *uploadSuite) trigger(correlationID string) {
+	params := map[string]interface{}{
+		correlationID: correlationID,
+	}
+	suite.execCommand(operationTrigger, params)
+}
+
+func (suite *uploadSuite) execCommand(command string, params map[string]interface{}) {
+	url := fmt.Sprintf("%s/inbox/messages/%s", suite.featureURL, command)
+	suite.doRequest(http.MethodPost, url, params)
+}
+
+func (suite *uploadSuite) awaitLastUpload(correlationID string, expectedState string) *lastUpload {
+	url := fmt.Sprintf("%s/properties/%s", suite.featureURL, propertyLastUpload)
+	for i := 0; i < uploadFilesTimeout; i++ {
+		body, err := suite.doRequest(http.MethodGet, url, nil)
+		if err == nil && len(body) > 0 {
+			lastUpload := &lastUpload{}
+			if err := json.Unmarshal(body, lastUpload); err == nil {
+				if correlationID == lastUpload.CorrelationId && isTerminal(lastUpload.State) {
+					return lastUpload
+				}
+			}
+		}
+		time.Sleep(time.Second)
+	}
+	return nil
+}
---
+++ integration/upload_test.go
@@ -0,0 +1,301 @@
+// Copyright (c) 2022 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+
+package integration
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/eclipse-kanto/file-upload/client"
+	"github.com/eclipse/ditto-clients-golang"
+	"github.com/eclipse/ditto-clients-golang/model"
+	"github.com/eclipse/ditto-clients-golang/protocol"
+	"github.com/eclipse/ditto-clients-golang/protocol/things"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type testConfig struct {
+	Broker                   string `def:"tcp://localhost:1883"`
+	MqttQuiesceMs            int    `def:"500"`
+	MqttAcknowledgeTimeoutMs int    `def:"3000"`
+
+	DittoAddress string
+
+	DittoUser     string `def:"ditto"`
+	DittoPassword string `def:"ditto"`
+
+	EventTimeoutMs  int `def:"30000"`
+	StatusTimeoutMs int `def:"10000"`
+
+	TimeDeltaMs int `def:"5000"`
+}
+
+type thingConfig struct {
+	DeviceID string `json:"deviceId"`
+	TenantID string `json:"tenantId"`
+	PolicyID string `json:"policyId"`
+}
+
+type uploadSuite struct {
+	suite.Suite
+
+	mqttClient  mqtt.Client
+	dittoClient *ditto.Client
+
+	cfg      *testConfig
+	thingCfg *thingConfig
+
+	thingURL   string
+	featureURL string
+}
+
+type uploadHandler interface {
+	prepare() error
+	getStartOptions(correlationID string, filePath string) map[string]interface{}
+	getContent(correlationID string) ([]byte, error)
+	dispose()
+}
+
+type lastUpload struct {
+	Progress      int    `json:"progress"`
+	CorrelationId string `json:"correlationId"`
+	StartTime     string `json:"startTime"`
+	State         string `json:"state"`
+	EndTime       string `json:"endTime"`
+	Message       string `json:"message"`
+	StatusCode    string `json:"statusCode"`
+}
+
+const (
+	envVariablesPrefix = "SCT"
+	featureID          = "AutoUploadable"
+
+	uploadFilesTimeout   = 20
+	uploadRequestTimeout = 10
+	uploadFilesPattern   = "upload_it_%d.txt"
+	uploadFilesCount     = 5
+
+	configFile         = "/etc/file-upload/config.json"
+	paramCorrelationID = "correlationID"
+	operationTrigger   = "trigger"
+	operationStart     = "start"
+	propertyLastUpload = "lastUpload"
+)
+
+var uploadDir string
+
+func (suite *uploadSuite) SetupSuite() {
+	cfg := &testConfig{}
+
+	suite.T().Log(getConfigHelp(*cfg, envVariablesPrefix))
+
+	if err := initConfigFromEnv(cfg, envVariablesPrefix); err != nil {
+		suite.T().Skip(err)
+	}
+
+	suite.T().Logf("test config: %+v", *cfg)
+
+	opts := mqtt.NewClientOptions().
+		AddBroker(cfg.Broker).
+		SetClientID(uuid.New().String()).
+		SetKeepAlive(30 * time.Second).
+		SetCleanSession(true).
+		SetAutoReconnect(true)
+
+	mqttClient := mqtt.NewClient(opts)
+
+	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		require.NoError(suite.T(), token.Error(), "connect to MQTT broker")
+	}
+
+	thingCfg, err := getThingConfig(mqttClient)
+	if err != nil {
+		mqttClient.Disconnect(uint(cfg.MqttQuiesceMs))
+		require.NoError(suite.T(), err, "get thing config")
+	}
+
+	suite.T().Logf("thing config: %+v", *thingCfg)
+
+	dittoClient, err := ditto.NewClientMQTT(mqttClient, ditto.NewConfiguration())
+	if err == nil {
+		err = dittoClient.Connect()
+	}
+
+	if err != nil {
+		mqttClient.Disconnect(uint(cfg.MqttQuiesceMs))
+		require.NoError(suite.T(), err, "initialize ditto client")
+	}
+
+	feature := &model.Feature{}
+	cmd := things.NewCommand(model.NewNamespacedIDFrom(thingCfg.DeviceID)).Twin().Feature(featureID).
+		Modify(feature)
+	msg := cmd.Envelope(protocol.WithResponseRequired(false))
+
+	err = dittoClient.Send(msg)
+	require.NoError(suite.T(), err, "create test feature")
+
+	suite.dittoClient = dittoClient
+	suite.mqttClient = mqttClient
+	suite.cfg = cfg
+	suite.thingCfg = thingCfg
+
+	suite.thingURL = fmt.Sprintf("%s/api/2/things/%s", strings.TrimSuffix(cfg.DittoAddress, "/"), thingCfg.DeviceID)
+	suite.featureURL = fmt.Sprintf("%s/features/%s", suite.thingURL, featureID)
+
+	uploadDir, err = setupUploadDir()
+	require.Nil(suite.T(), err, "get configured file upload directory")
+	require.NotEmpty(suite.T(), uploadDir, "get configured file upload directory")
+	suite.T().Logf("upload dir - %s", uploadDir)
+}
+
+func (suite *uploadSuite) TearDownSuite() {
+	suite.dittoClient.Disconnect()
+	suite.mqttClient.Disconnect(uint(suite.cfg.MqttQuiesceMs))
+}
+
+func setupUploadDir() (string, error) {
+	var uploadDir string
+	data, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return uploadDir, err
+	}
+	config := make(map[string]interface{})
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return uploadDir, err
+	}
+	if files, ok := config["files"]; ok {
+		uploadDir = filepath.Dir(files.(string))
+	}
+	return uploadDir, nil
+}
+
+func TestFileUpload(t *testing.T) {
+	suite.Run(t, new(uploadSuite))
+}
+
+func (suite *uploadSuite) TestUploadHTTP() {
+	suite.T().Log("test file upload over HTTP")
+	uploadHandler := newHTTPUploadHandler()
+	suite.testUpload(uploadHandler)
+}
+
+func (suite *uploadSuite) testUpload(uploadHandler uploadHandler) {
+	files, err := createTestFiles(uploadDir)
+	defer deleteTestFiles(files)
+	require.Nil(suite.T(), err, "create test files")
+
+	err = uploadHandler.prepare()
+	require.Nil(suite.T(), err, "prepare upload handler")
+	defer uploadHandler.dispose()
+
+	type uploadRequest struct {
+		CorrelationID string            `json:"correlationId"`
+		Options       map[string]string `json:"options"`
+	}
+	requestPath := fmt.Sprintf("/features/%s/outbox/messages/request", featureID)
+	filePaths := make(map[string]string)
+	eventTopic := "e"
+	suite.mqttClient.Subscribe(eventTopic, 1, func(c mqtt.Client, m mqtt.Message) {
+		res := &protocol.Envelope{}
+		err := json.Unmarshal(m.Payload(), &res)
+		if err != nil {
+			return // skip
+		}
+		if res.Path == requestPath {
+			jsonValue, err := json.Marshal(res.Value)
+			if err != nil {
+				return // skip
+			}
+			uploadRequest := &uploadRequest{}
+			if err = json.Unmarshal([]byte(jsonValue), uploadRequest); err != nil {
+				return // skip
+			}
+			if path, ok := uploadRequest.Options["file.path"]; ok {
+				suite.T().Logf("file upload request: %s, with correlation id: %s", path, uploadRequest.CorrelationID)
+				filePaths[uploadRequest.CorrelationID] = path
+			}
+		}
+	})
+	defer suite.mqttClient.Unsubscribe(eventTopic)
+
+	correlationId := "test"
+	suite.trigger(correlationId)
+	time.Sleep(uploadRequestTimeout * time.Second)
+	suite.T().Logf("%v file upload requests, initiating uploads", len(filePaths))
+
+	filePathsRev := make(map[string]string)
+	for startId, path := range filePaths {
+		filePathsRev[path] = startId
+		suite.execCommand(operationStart, uploadHandler.getStartOptions(startId, path))
+	}
+	suite.assertUploadSuccessful(correlationId)
+	for _, filePath := range files {
+		startId, ok := filePathsRev[filePath]
+		require.True(suite.T(), ok, "upload request events")
+		content, err := uploadHandler.getContent(startId)
+		require.Nil(suite.T(), err, "uploaded files")
+		suite.compareContent(filePath, content)
+	}
+}
+
+func (suite *uploadSuite) compareContent(filePath string, received []byte) {
+	expected, err := ioutil.ReadFile(filePath)
+	require.Nil(suite.T(), err, "uploaded files")
+	require.Equal(suite.T(), expected, received, "uploaded files")
+}
+
+func isTerminal(state string) bool {
+	return state == client.StateSuccess || state == client.StateFailed || state == client.StateCanceled
+}
+
+func (suite *uploadSuite) assertUploadSuccessful(correlationID string) {
+	lastUpload := suite.awaitLastUpload(correlationID, client.StateSuccess)
+	suite.T().Logf("last upload value - %v", lastUpload)
+	require.NotNil(suite.T(), lastUpload, "last upload property value")
+	require.Equal(suite.T(), client.StateSuccess, lastUpload.State, "last upload state")
+	require.Equal(suite.T(), 100, lastUpload.Progress, "last upload progress")
+}
+
+func createTestFiles(dir string) ([]string, error) {
+	var result []string
+	for i := 1; i <= uploadFilesCount; i++ {
+		filePath := filepath.Join(dir, fmt.Sprintf(uploadFilesPattern, i))
+		result = append(result, filePath)
+		if err := writeTestContent(filePath, 10*i); err != nil {
+			return result, err
+		}
+	}
+	return result, nil
+}
+
+func deleteTestFiles(files []string) {
+	for _, file := range files {
+		os.Remove(file)
+	}
+}
+
+func writeTestContent(filePath string, count int) error {
+	data := strings.Repeat("test", count)
+	return ioutil.WriteFile(filePath, []byte(data), fs.ModePerm)
+}
